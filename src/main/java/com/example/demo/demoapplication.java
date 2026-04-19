64858acaa1ce8d532f85b1346e851654a57649db
Darkhunt16 <chandrasandeep0209@gmail.com>
2026-04-19 19:32:46 +0530

    change demo

--- src/main/java/com/example/demo/demoapplication.java
+++ src/main/java/com/example/demo/demoapplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class demoapplication {
 
     @GetMapping("/")
     public String home() {
